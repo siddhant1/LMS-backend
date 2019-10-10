36b6f3e16dfaf6205443543a2bd8f2c30cd01eab
siddhant1 <sidhant.manchanda@gmail.com>
2019-10-10 18:48:29 +0530

    updated auth logic

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,15 +3,15 @@ function auth(req, res, next, shouldThrow = true) {
   const token = req.header("x-auth-token")
   if (!shouldThrow && !token) {
     next()
-  }
-  else if(!shouldThrow && token){
-    try{
+  } else if (!shouldThrow && token) {
+    try {
       const payload = jwt.verify(token, process.env.jwtPrivateKey)
-      req.user = payload;
-      next();
+      req.user = payload
+      next()
+    } catch (e) {
+      next()
+      return
     }
-    catch (e) {
-      res.status(401).send("Invalid Token")
   }
   if (!token) return res.status(401).send("Access Denied No Token Found")
   try {
